4f78d9b675b9917217cf78de3f3d753b99df863c
NishchalSrinarayanan <nishchalstudios@gmail.com>
2024-07-17 11:12:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ if symbol:
         data = response.json()
 
         if "Global Quote" in data:
+            try:
                 price = float(data["Global Quote"]["05. price"])
                 yest_price = float(data["Global Quote"]["08. previous close"])
 
